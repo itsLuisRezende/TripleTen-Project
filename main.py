d6aaf275b29b623508b104c584ff48bd28f16825
Luis Fernando <140192412+itsLuisRezende@users.noreply.github.com>
2025-06-28 18:41:20 -0300

    add gitignore!

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('PASS')
+        print('PASS!')
     else:
-        print('FAIL')
+        print('FAIL!')
 
 in_autotests_we_trust(10, '10')
 
